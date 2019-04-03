ba352248eae80ed56802e899728df9255db72fdb
Vladislav Ilin <vladislav.ilin@hiq.ru>
2019-04-03 11:51:17 +0300

    extruct id from the route in the edit mode

--- src/app/skills/skills-create/skills-create.component.ts
+++ src/app/skills/skills-create/skills-create.component.ts
@@ -1,17 +1,35 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Skill } from "../skill.model";
 import { SkillsService } from "../skills.service";
+import { ActivatedRoute, ParamMap } from "@angular/router";
 
 @Component({
   selector: "app-skills-create",
   templateUrl: "./skills-create.component.html",
   styleUrls: ["./skills-create.component.css"]
 })
-export class SkillsCreateComponent {
+export class SkillsCreateComponent implements OnInit {
   // newSkill = "No content";
+  private mode = "create";
+  private postId: string;
 
-  constructor(public skillsService: SkillsService) {}
+  constructor(
+    public skillsService: SkillsService,
+    public route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has("postId")) {
+        this.mode = "edit";
+        this.postId = paramMap.get("postId");
+      } else {
+        this.mode = "create";
+        this.postId = null;
+      }
+    });
+  }
 
   onAddSkill(form: NgForm) {
     if (form.invalid) {
